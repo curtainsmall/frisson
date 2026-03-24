52ee0bbaaf72fe0626cf4ad332197e5f4d96a14d
curtainsmall <Steve Dong>
2026-03-24 22:32:03 +0800

    Created basic window;

--- src/CoyoteStudio.App/ViewModels/MainWindowViewModel.cs
+++ src/CoyoteStudio.App/ViewModels/MainWindowViewModel.cs
@@ -1,6 +1,16 @@
-﻿namespace CoyoteStudio.App.ViewModels;
+﻿using System.Collections.ObjectModel;
+
+using CoyoteStudio.Shared;
+
+namespace CoyoteStudio.App.ViewModels;
 
 public partial class MainWindowViewModel : ViewModelBase
 {
+    public ObservableCollection<ClientDto> ConnectedClients { get; } = new();
     public string Greeting { get; } = "Welcome to Avalonia!";
+
+    public MainWindowViewModel()
+    {
+        ConnectedClients.Add(new ClientDto { ClientName = "localhost" });
+    }
 }
---
+++ src/CoyoteStudio.Shared/ClientDto.cs
@@ -0,0 +1,8 @@
+﻿namespace CoyoteStudio.Shared
+{
+    public class ClientDto
+    {
+        public string? ClientName { get; set; }
+        public string? ConnectionTime { get; set; }
+    }
+}
